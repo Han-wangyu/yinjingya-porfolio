6adc45b9db8aa0e4d9a0609905d4e8ff4ce7cf2b
hanwangyu <hanwangyu@foxmail.com>
2023-12-17 17:41:50 +0800

    Initial Commit

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'YinJingya(尹静雅) | Homepage';
+export const SITE_DESCRIPTION = 'YinJingya is looking for a Ph.D. in Atmospheric Sciences. I am a junior undergraduate student at Nanjing University of Information Science and Technology(NUIST), majoring in Atmospheric Sciences. YinJingya has some research experience in integrating multi-source big data for atmospheric prediction, applying machine learning in meteorology, and conducting numerical modeling of weather and climate events. Yinjingya is eager to expand my knowledge into other fields and is currently seeking remote research or summer research opportunities.';
